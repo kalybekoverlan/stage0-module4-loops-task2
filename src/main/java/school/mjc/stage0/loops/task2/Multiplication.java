8d17343fcd28eab164e6d1c4ca0f620137b5a669
erlan kalybekov <kalybekov.erlan@gmail.com>
2022-07-05 04:50:01 +0600

    solved

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int counter = 0;
+        int multiplication;
+
+        if(multiplyByAndToInclusive > 0) {
+            while (counter <= multiplyByAndToInclusive) {
+                multiplication = counter * multiplyByAndToInclusive;
+                System.out.println(multiplication);
+                counter++;
+            }
+        }
+        if(multiplyByAndToInclusive < 0) {
+            while (counter <= multiplyByAndToInclusive * (-1)) {
+                multiplication = counter * multiplyByAndToInclusive;
+                System.out.println(multiplication);
+                counter++;
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int counter = 1;
+        int result = 1;
 
+        if(power < 0) {
+            System.out.println("too much power");
+        } else {
+            System.out.println(1);
+            while (counter <= power) {
+                result = result * 2;
+                System.out.println(result);
+                counter++;
+            }
+        }
     }
 }
